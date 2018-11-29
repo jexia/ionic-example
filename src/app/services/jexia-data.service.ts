e9e5dd78a6c8fb15f2ea779cba6f4a4c82a727ab
DESKTOP-72NU8U5\Omnia <nandekonst1@gmail.com>
2018-11-29 15:32:39 +0100

    changed initial value BehaviorSubject from null to []

--- src/app/services/jexia-data.service.ts
+++ src/app/services/jexia-data.service.ts
@@ -4,6 +4,7 @@ import { Dataset } from 'jexia-sdk-js/api/dataops/dataset';
 import { DataOperations } from '@jexia/ng-jexia';
 import { field } from 'jexia-sdk-js/api/dataops/filteringApi';
 import { ITeacher } from '../interfaces/ITeacher';
+import { ILocation } from '../interfaces/ILocation';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -15,14 +16,15 @@ export class JexiaDataService {
   //select it and execute it 
   teachers: Promise<ITeacher[]> = this.teacherdataset.select().execute();
   //initalize behaviorsubject, default value is null
-  teacherSource: BehaviorSubject<ITeacher[]> = new BehaviorSubject<ITeacher[]>(null);
+  teacherSource: BehaviorSubject<ITeacher[]> = new BehaviorSubject<ITeacher[]>([]);
   //using only the observable side of the behaviorsubject and let other components subscribe
   currentMessage: Observable<ITeacher[]> = this.teacherSource.asObservable();
 
  constructor(public dataOperations: DataOperations) {   }
+ 
 
- updateTeachers(teacherobj: any): void {
-  let filterCondition = field("location").isEqualTo(teacherobj.city)
+ updateTeachers(location: ILocation): void {  
+  let filterCondition = field("location").isEqualTo(location.city)
   this.teacherdataset.select().where(filterCondition).execute().then(data => {
     //sending out the messagestream
     this.teacherSource.next(data);
